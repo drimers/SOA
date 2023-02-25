587bf2917d9cf67c52d4ff7d28a616184340eb61
Stefan Petkov <spp.bg@abv.bg>
2023-02-25 21:48:03 +0200

    client changes sys.out

--- src/main/java/client/StudentClient.java
+++ src/main/java/client/StudentClient.java
@@ -14,54 +14,56 @@ public class StudentClient {
 
 	public static void main(String[] args) throws RemoteException {
 		// TODO Auto-generated method stub
-//		
+
+		StudentServiceStub stub = new StudentServiceStub();
+
+		GetAllStudents param = new GetAllStudents();
+		GetAllStudentsResponse allStudentsResponce = stub.getAllStudents(param);
+
+		Student[] allStudents = allStudentsResponce.get_return();
+
+		for (Student student : allStudents) {
+			System.out.println("FNumber : [" + student.getFacultyNumber() + "] Name : [" + student.getName()
+					+ "] Specialty : [" + student.getSpeciality() + "]");
+		}
+
+		GetStudent param2 = new GetStudent();
+
+		param2.setFacultyNumber("1234567");
+
+		GetStudentResponse response2 = stub.getStudent(param2);
+
+		Student student2 = response2.get_return();
+
+		System.out.println("##################################################################");
+		System.out.println("FNumber : [" + student2.getFacultyNumber() + "] Name : [" + student2.getName()
+				+ "] Specialty : [" + student2.getSpeciality() + "]");
+
+	
+		
+		
 //		StudentServiceStub stub = new StudentServiceStub();
-//	
+//
 //		GetAllStudents param = new GetAllStudents();
-//		GetAllStudentsResponse allStudentsResponce = stub.getAllStudents(param);
-//		
-//		Student[] allStudents = allStudentsResponce.get_return();
-//		
-//		for(Student student : allStudents) {
-//			System.out.println(student.getFacultyNumber() + student.getSpeciality() + student.getName());
+//
+//		GetAllStudentsResponse response = stub.getAllStudents(param);
+//
+//		Student[] students = response.get_return();
+//
+//		for (Student student : students) {
+//			System.out.printf("%s\t%s\t%s%n", student.getFacultyNumber(), student.getName(), student.getSpeciality());
 //		}
-//		
-//		
+//
 //		GetStudent param2 = new GetStudent();
-//		
 //		param2.setFacultyNumber("1234567");
-//		
-//		
+//
 //		GetStudentResponse response2 = stub.getStudent(param2);
-//		
-//		Student student = new Student();
-		
-		
-		 StudentServiceStub stub = new StudentServiceStub();
+//
+//		Student student2 = response2.get_return();
+//
+//		System.out.println("====================================");
+//		System.out.printf("%s\t%s\t%s%n", student2.getFacultyNumber(), student2.getName(), student2.getSpeciality());
 
-	        GetAllStudents param = new GetAllStudents();
-
-	        GetAllStudentsResponse response = stub.getAllStudents(param);
-
-	        Student[] students = response.get_return();
-
-	        for (Student student: students) {
-	            System.out.printf("%s\t%s\t%s%n", student.getFacultyNumber(), student.getName(), student.getSpeciality());
-	        }
-
-	        GetStudent param2 = new GetStudent();
-	        param2.setFacultyNumber("1234567");
-
-	        GetStudentResponse response2 = stub.getStudent(param2);
-
-	        Student student2 = response2.get_return();
-
-	        System.out.println("====================================");
-	        System.out.printf("%s\t%s\t%s%n", student2.getFacultyNumber(), student2.getName(), student2.getSpeciality());
-
-		
-		
-		
 	}
 
 }
